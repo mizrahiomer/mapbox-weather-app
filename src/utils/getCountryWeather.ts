b01acb788bae96d1efdc35dac19714881c874d22
mizrahiomer <omermiz1604@gmail.com>
2020-06-01 14:35:21 +0300

    test

--- src/utils/getCountryWeather.ts
+++ src/utils/getCountryWeather.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getCountryWeather = async (lat: number, lon: number) => {
 	const res = await axios.get(
-		`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&&units=metric&appid=${process.env.REACT_APP_OW_KEY}`
+		`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&&units=metric&appid=${process.env.REACT_APP_OW_KEY}`
 	);
 	return res.data.main;
 };
